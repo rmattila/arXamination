2883c7615c4b3406f807a2f715168b136c849519
Robert <robfrog@gmail.com>
2023-11-03 10:54:17 +0100

    add requirements.txt

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@ setup(
     name="arxamination",
     version="0.1",
     packages=find_packages(),
-    install_requires=[
-        # Add your dependencies here
-    ],
     entry_points={
         "console_scripts": [
             "arxamination = arxamination.main:main",
