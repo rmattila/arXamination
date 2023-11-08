479322b8eff63482c378487b5fb0716f24515fd8
Robert <robfrog@gmail.com>
2023-11-08 08:35:55 +0100

    add option to sample a subset of questions

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import random
 from tqdm import tqdm
 
 from arxamination import arxiv_parser
@@ -7,28 +8,44 @@ from arxamination.utils import load_config_file, make_bold
 
 
 def main():
+    # Parse command line arguments
     parser = argparse.ArgumentParser(description="Fetch and analyze arXiv articles.")
     parser.add_argument("arxiv_id", help="The arXiv article ID (e.g., '1706.03762')")
+    parser.add_argument(
+        "--num_questions",
+        "-n",
+        type=int,
+        default=None,
+        help="Number of questions to sample",
+    )
+
     args = parser.parse_args()
     arxiv_id = args.arxiv_id
+    num_questions = args.num_questions
 
+    # Fetch and read PDF
     pdf_text = arxiv_parser.process_arxiv(arxiv_id)
 
     if not pdf_text:
         print(f"Failed to load text of {arxiv_id}")
         return
 
+    # Load config and LLM
     config_file = "config.json"
     llm = LocalLLM(config_file)
     config = load_config_file(config_file)
 
     print(f"Using model {config['model_choice']}")
 
-    questions = config["questions"]
+    # If num_questions has not been set, go through all questions; otherwise, sample num_questions
+    if num_questions is None:
+        questions = config["questions"]
+    else:
+        questions = random.sample(config["questions"], num_questions)
 
     with tqdm(questions, desc="Processing Questions") as pbar:
         for question in pbar:
-            # format the question for display in the progress bar
+            # Format the question for display in the progress bar
             formatted_question = (
                 (question[:32] + "...?") if len(question) > 35 else question
             )
@@ -37,7 +54,7 @@ def main():
 
             summary = llm.process_with_llm(pdf_text, question)
 
-            # print the question and summary
+            # Print the question and summary
             tqdm.write("\n")
             tqdm.write(make_bold(question))
             tqdm.write("-" * len(question))
